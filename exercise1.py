7d6349edec10482b9359938359f2b8f5c70f0248
tszgg <62896796+tszgg@users.noreply.github.com>
2020-05-19 22:10:43 +0800

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -13,9 +13,22 @@ data = {
     'David': 'Biology,Chinese'
 }
 
+trim_data={}
+for k, v in data.items():
+    new_v = list(map(str,v.replace(' ','').split(',')))
+    trim_data[k]=new_v
+    data=trim_data
 
 def reverse_dict(input_data):
     # TODO
+    output = {}
+    for k in input_data:
+        for v in input_data[k]:
+            if v in output:
+                output[v].append(k)
+            else:
+                output[v]=[k]
+           #output.setdefault(v, []).append(k)
     return output  # TODO
 
 
